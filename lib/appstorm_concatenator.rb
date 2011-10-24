fd495bb349cd64792c1c8b62cf20d60768802e21
Ian Murray <ianmurrays@gmail.com>
2011-10-24 12:25:06 -0300

    Commiting the code

--- lib/appstorm_concatenator.rb
+++ lib/appstorm_concatenator.rb
@@ -1,5 +1,28 @@
 require "appstorm_concatenator/version"
+require "RMagick"
 
 module AppstormConcatenator
-  # Your code goes here...
+  include Magick
+  
+  def concatenate_images(image_1, image_2, output)
+    # Create the canvas
+    canvas = Image.new(620, 465) { self.background_color = "black" }
+
+    # Images
+    puts "--> Loading #{ARGV[0]}"
+    img = Image.read(ARGV[0]).first
+
+    puts "--> Loading #{ARGV[1]}"
+    img2 = Image.read(ARGV[1]).first
+
+    puts "--> Resizing images"
+    img.resize! 305, 458
+    img2.resize! 305, 458
+
+    puts "--> Creating final image #{ARGV[2]}"
+    canvas.composite(img, 3, 3, OverCompositeOp).composite(img2, 312, 3, OverCompositeOp).write(ARGV[2])
+
+    puts "--> Done!"
+    system "open #{ARGV[2]}" if !!(RUBY_PLATFORM =~ /darwin/)
+  end
 end
